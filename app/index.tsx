90d0d6d5f93f66c7def130528cfbd3baa6dbcbd2
steveacalabro-indevets <150180143+steveacalabro-indevets@users.noreply.github.com>
2025-01-15 16:36:16 -0500

    added safe area view

--- app/index.tsx
+++ app/index.tsx
@@ -2,15 +2,18 @@ import { View } from 'react-native';
 import { Button } from 'chameleon-ui'
 import { TamaguiProvider } from 'tamagui';
 import config from 'chameleon-ui/dist/lib/tamagui.config';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
   return (
-    <TamaguiProvider config={config} defaultTheme="light">
-      <View>
-        <Button title='Test' onPress={() => {
-          console.log('This works')
-        }} />
-      </View>
-    </TamaguiProvider>
+    <SafeAreaView>
+      <TamaguiProvider config={config} defaultTheme="light">
+        <View>
+          <Button title='Test' onPress={() => {
+            console.log('This works')
+          }} />
+        </View>
+      </TamaguiProvider>
+    </SafeAreaView>
   );
 }
